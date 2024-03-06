37d4de663ffcd889c85bbe3a83de1cefc229a7a7
Michael Haberler <git@mah.priv.at>
2024-03-06 17:10:32 +0100

    Austria DEM test

---
+++ cmd/at-elevation-test.cmd
@@ -0,0 +1,27 @@
+# memory status
+mem
+
+# use Austria Elevation Model
+map DTM_Austria_10m_v2_by_Sonny_3857_RGB_13_webp.pmtiles
+
+# 8113 Stiwoll Kehrer - reference 865.8m
+ele 47.12925176802318 15.209778656353123
+
+# 1180 Utopiaweg 1 - reference 333.0m
+ele 48.2383409011934 16.299522929921253
+
+# 1190 Höhenstraße - reference 403.6m
+ele 48.2610837936095 16.289583084029545
+
+# 1010 Stephansplatz - reference 171.4m
+ele 48.208694143314325 16.37255104738311
+
+# 1220 Industriestraße 81 - reference 158.6m
+ele 48.225003606677504 16.44120643847108
+
+# list cached tiles
+lc
+
+# memory status
+mem
+
